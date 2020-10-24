1868bcd2cea6ce7068ee8991400b9ec5dc1bf915
bcace <branimir.cace@gmail.com>
2020-10-25 00:11:19 +0200

    cleanup

--- src/tay/tree.c
+++ src/tay/tree.c
@@ -34,64 +34,64 @@ typedef struct {
     unsigned char dims[TAY_MAX_DIMENSIONS];
 } Depths;
 
-typedef struct Node {
-    struct Node *lo, *hi;               /* child partitions */
+typedef struct Cell {
+    struct Cell *lo, *hi;               /* child partitions */
     TayAgentTag *first[TAY_MAX_GROUPS]; /* agents contained in this node (fork or leaf) */
     int dim;                            /* dimension along which the node's partition is divided into child partitions */
     Box box;
-} Node;
+} Cell;
 
-static void _clear_node(Node *node) {
-    node->lo = 0;
-    node->hi = 0;
+static void _clear_cell(Cell *cell) {
+    cell->lo = 0;
+    cell->hi = 0;
     for (int i = 0; i < TAY_MAX_GROUPS; ++i)
-        node->first[i] = 0;
+        cell->first[i] = 0;
 }
 
 typedef struct {
     TayAgentTag *first[TAY_MAX_GROUPS]; /* agents are kept here while not in nodes, will be sorted into the tree at next step */
-    Node *nodes;                        /* nodes storage, first node is always the root node */
-    int nodes_cap;
-    int available_node;
+    Cell *cells;                        /* nodes storage, first node is always the root node */
+    int max_cells;
+    int available_cell;
     int dims;
     int max_depth_correction;
     float radii[TAY_MAX_DIMENSIONS];
     int max_depths[TAY_MAX_DIMENSIONS];
     Box box;
-} Tree;
+} Space;
 
-static Tree *_init(int dims, float *radii, int max_depth_correction) {
-    Tree *t = calloc(1, sizeof(Tree));
-    t->dims = dims;
-    t->max_depth_correction = max_depth_correction;
-    t->nodes_cap = 100000;
-    t->nodes = malloc(t->nodes_cap * sizeof(Node));
-    t->available_node = 1; /* root node is always allocated */
-    _clear_node(t->nodes);
-    _reset_box(&t->box, t->dims);
+static Space *_init(int dims, float *radii, int max_depth_correction) {
+    Space *s = calloc(1, sizeof(Space));
+    s->dims = dims;
+    s->max_depth_correction = max_depth_correction;
+    s->max_cells = 100000;
+    s->cells = malloc(s->max_cells * sizeof(Cell));
+    s->available_cell = 1; /* root node is always allocated */
+    _clear_cell(s->cells);
+    _reset_box(&s->box, s->dims);
     for (int i = 0; i < dims; ++i)
-        t->radii[i] = radii[i];
-    return t;
+        s->radii[i] = radii[i];
+    return s;
 }
 
 static void _destroy(TaySpaceContainer *container) {
-    Tree *t = container->storage;
-    free(t->nodes);
-    free(t);
+    Space *s = container->storage;
+    free(s->cells);
+    free(s);
 }
 
-static void _add_to_non_sorted(Tree *tree, TayAgentTag *agent, int group) {
-    agent->next = tree->first[group];
-    tree->first[group] = agent;
-    _update_box(&tree->box, TAY_AGENT_POSITION(agent), tree->dims);
+static void _add_to_non_sorted(Space *space, TayAgentTag *agent, int group) {
+    agent->next = space->first[group];
+    space->first[group] = agent;
+    _update_box(&space->box, TAY_AGENT_POSITION(agent), space->dims);
 }
 
 static void _add(TaySpaceContainer *container, TayAgentTag *agent, int group, int index) {
-    Tree *t = container->storage;
-    _add_to_non_sorted(t, agent, group);
+    Space *s = container->storage;
+    _add_to_non_sorted(s, agent, group);
 }
 
-static void _move_agents_from_node_to_tree(Tree *tree, Node *node) {
+static void _move_agents_from_node_to_tree(Space *space, Cell *node) {
     if (node == 0)
         return;
     for (int i = 0; i < TAY_MAX_GROUPS; ++i) {
@@ -100,21 +100,21 @@ static void _move_agents_from_node_to_tree(Tree *tree, Node *node) {
         TayAgentTag *last = node->first[i];
         /* update global extents and find the last agent */
         while (1) {
-            _update_box(&tree->box, TAY_AGENT_POSITION(last), tree->dims);
+            _update_box(&space->box, TAY_AGENT_POSITION(last), space->dims);
             if (last->next)
                 last = last->next;
             else
                 break;
         }
         /* add all agents to global list */
-        last->next = tree->first[i];
-        tree->first[i] = node->first[i];
+        last->next = space->first[i];
+        space->first[i] = node->first[i];
     }
-    _move_agents_from_node_to_tree(tree, node->lo);
-    _move_agents_from_node_to_tree(tree, node->hi);
+    _move_agents_from_node_to_tree(space, node->lo);
+    _move_agents_from_node_to_tree(space, node->hi);
 }
 
-static inline void _decide_node_split(Node *node, int dims, int *max_depths, float *radii, Depths node_depths) {
+static inline void _decide_node_split(Cell *node, int dims, int *max_depths, float *radii, Depths node_depths) {
     node->dim = NODE_DIM_LEAF;
     float max_r = 0.0f;
     for (int i = 0; i < dims; ++i) {
@@ -128,7 +128,7 @@ static inline void _decide_node_split(Node *node, int dims, int *max_depths, flo
     }
 }
 
-static void _sort_agent(Tree *tree, Node *node, TayAgentTag *agent, int group, Depths node_depths) {
+static void _sort_agent(Space *space, Cell *node, TayAgentTag *agent, int group, Depths node_depths) {
     if (node->dim == NODE_DIM_LEAF) {
         agent->next = node->first[group];
         node->first[group] = agent;
@@ -141,25 +141,25 @@ static void _sort_agent(Tree *tree, Node *node, TayAgentTag *agent, int group, D
         float pos = TAY_AGENT_POSITION(agent)[node->dim];
         if (pos < mid) {
             if (node->lo == 0) {
-                assert(tree->available_node < tree->nodes_cap);
-                node->lo = tree->nodes + tree->available_node++;
-                _clear_node(node->lo);
+                assert(space->available_cell < space->max_cells);
+                node->lo = space->cells + space->available_cell++;
+                _clear_cell(node->lo);
                 node->lo->box = node->box;
                 node->lo->box.max[node->dim] = mid;
-                _decide_node_split(node->lo, tree->dims, tree->max_depths, tree->radii, sub_node_depths);
+                _decide_node_split(node->lo, space->dims, space->max_depths, space->radii, sub_node_depths);
             }
-            _sort_agent(tree, node->lo, agent, group, sub_node_depths);
+            _sort_agent(space, node->lo, agent, group, sub_node_depths);
         }
         else {
             if (node->hi == 0) {
-                assert(tree->available_node < tree->nodes_cap);
-                node->hi = tree->nodes + tree->available_node++;
-                _clear_node(node->hi);
+                assert(space->available_cell < space->max_cells);
+                node->hi = space->cells + space->available_cell++;
+                _clear_cell(node->hi);
                 node->hi->box = node->box;
                 node->hi->box.min[node->dim] = mid;
-                _decide_node_split(node->hi, tree->dims, tree->max_depths, tree->radii, sub_node_depths);
+                _decide_node_split(node->hi, space->dims, space->max_depths, space->radii, sub_node_depths);
             }
-            _sort_agent(tree, node->hi, agent, group, sub_node_depths);
+            _sort_agent(space, node->hi, agent, group, sub_node_depths);
         }
     }
 }
@@ -174,69 +174,70 @@ static int _max_depth(float space_side, float see_side, int max_depth_correction
 }
 
 static void _on_step_start(TayState *state) {
-    Tree *t = state->space.storage;
-    _move_agents_from_node_to_tree(t, t->nodes);
+    Space *s = state->space.storage;
+
+    _move_agents_from_node_to_tree(s, s->cells);
 
     /* calculate max partition depths for each dimension */
     Depths root_node_depths;
-    for (int i = 0; i < t->dims; ++i) {
-        t->max_depths[i] = _max_depth(t->box.max[i] - t->box.min[i], t->radii[i] * 2.0f, t->max_depth_correction);
+    for (int i = 0; i < s->dims; ++i) {
+        s->max_depths[i] = _max_depth(s->box.max[i] - s->box.min[i], s->radii[i] * 2.0f, s->max_depth_correction);
         root_node_depths.dims[i] = 0;
     }
 
     /* set up root node */
-    t->available_node = 1;
-    _clear_node(t->nodes);
-    t->nodes->box = t->box; /* root node inherits space's box */
-    _decide_node_split(t->nodes, t->dims, t->max_depths, t->radii, root_node_depths);
+    s->available_cell = 1;
+    _clear_cell(s->cells);
+    s->cells->box = s->box; /* root node inherits space's box */
+    _decide_node_split(s->cells, s->dims, s->max_depths, s->radii, root_node_depths);
 
     /* sort all agents from tree into nodes */
     for (int i = 0; i < TAY_MAX_GROUPS; ++i) {
-        TayAgentTag *next = t->first[i];
+        TayAgentTag *next = s->first[i];
         while (next) {
             TayAgentTag *a = next;
             next = next->next;
-            _sort_agent(t, t->nodes, a, i, root_node_depths);
+            _sort_agent(s, s->cells, a, i, root_node_depths);
         }
-        t->first[i] = 0;
+        s->first[i] = 0;
     }
 
-    _reset_box(&t->box, t->dims);
+    _reset_box(&s->box, s->dims);
 }
 
 typedef struct {
-    Tree *tree;
+    Space *space;
     TayPass *pass;
     int counter; /* tree node skipping counter so each thread processes different seer nodes */
-} TreeSeeTask;
+} SeeTask;
 
-static void _init_tree_see_task(TreeSeeTask *t, Tree *tree, TayPass *pass, int thread_index) {
-    t->tree = tree;
+static void _init_tree_see_task(SeeTask *t, Space *space, TayPass *pass, int thread_index) {
+    t->space = space;
     t->pass = pass;
     t->counter = thread_index;
 }
 
-static void _thread_traverse_seen(Tree *tree, Node *seer_node, Node *seen_node, TayPass *pass, Box seer_box, TayThreadContext *thread_context) {
-    for (int i = 0; i < tree->dims; ++i)
+static void _thread_traverse_seen(Space *space, Cell *seer_node, Cell *seen_node, TayPass *pass, Box seer_box, TayThreadContext *thread_context) {
+    for (int i = 0; i < space->dims; ++i)
         if (seer_box.min[i] > seen_node->box.max[i] || seer_box.max[i] < seen_node->box.min[i])
             return;
     if (seen_node->first[pass->seen_group]) /* if there are any "seen" agents */
-        tay_see(seer_node->first[pass->seer_group], seen_node->first[pass->seen_group], pass->see, pass->radii, tree->dims, thread_context);
+        tay_see(seer_node->first[pass->seer_group], seen_node->first[pass->seen_group], pass->see, pass->radii, space->dims, thread_context);
     if (seen_node->lo)
-        _thread_traverse_seen(tree, seer_node, seen_node->lo, pass, seer_box, thread_context);
+        _thread_traverse_seen(space, seer_node, seen_node->lo, pass, seer_box, thread_context);
     if (seen_node->hi)
-        _thread_traverse_seen(tree, seer_node, seen_node->hi, pass, seer_box, thread_context);
+        _thread_traverse_seen(space, seer_node, seen_node->hi, pass, seer_box, thread_context);
 }
 
-static void _thread_traverse_seers(TreeSeeTask *task, Node *node, TayThreadContext *thread_context) {
+static void _thread_traverse_seers(SeeTask *task, Cell *node, TayThreadContext *thread_context) {
     if (task->counter % runner.count == 0) {
         if (node->first[task->pass->seer_group]) { /* if there are any "seeing" agents */
             Box seer_box = node->box;
-            for (int i = 0; i < task->tree->dims; ++i) {
+            for (int i = 0; i < task->space->dims; ++i) {
                 seer_box.min[i] -= task->pass->radii[i];
                 seer_box.max[i] += task->pass->radii[i];
             }
-            _thread_traverse_seen(task->tree, node, task->tree->nodes, task->pass, seer_box, thread_context);
+            _thread_traverse_seen(task->space, node, task->space->cells, task->pass, seer_box, thread_context);
         }
     }
     ++task->counter;
@@ -246,18 +247,18 @@ static void _thread_traverse_seers(TreeSeeTask *task, Node *node, TayThreadConte
         _thread_traverse_seers(task, node->hi, thread_context);
 }
 
-static void _thread_see_traverse(TreeSeeTask *task, TayThreadContext *thread_context) {
-    _thread_traverse_seers(task, task->tree->nodes, thread_context);
+static void _thread_see_traverse(SeeTask *task, TayThreadContext *thread_context) {
+    _thread_traverse_seers(task, task->space->cells, thread_context);
 }
 
 static void _see(TayState *state, int pass_index) { // TaySpaceContainer *container, TayPass *pass) {
-    Tree *t = state->space.storage; // container->storage;
+    Space *t = state->space.storage; // container->storage;
     TayPass *p = state->passes + pass_index;
 
-    static TreeSeeTask tasks[TAY_MAX_THREADS];
+    static SeeTask tasks[TAY_MAX_THREADS];
 
     for (int i = 0; i < runner.count; ++i) {
-        TreeSeeTask *task = tasks + i;
+        SeeTask *task = tasks + i;
         _init_tree_see_task(task, t, p, i);
         tay_thread_set_task(i, _thread_see_traverse, task, p->context);
     }
@@ -268,26 +269,26 @@ static void _see(TayState *state, int pass_index) { // TaySpaceContainer *contai
 typedef struct {
     TayPass *pass;
     TayAgentTag *agents;
-} TreeActTask;
+} ActTask;
 
-static void _init_tree_act_task(TreeActTask *t, TayPass *pass, TayAgentTag *agents) {
+static void _init_tree_act_task(ActTask *t, TayPass *pass, TayAgentTag *agents) {
     t->pass = pass;
     t->agents = agents;
 }
 
-static void _act_func(TreeActTask *t, TayThreadContext *thread_context) {
+static void _act_func(ActTask *t, TayThreadContext *thread_context) {
     for (TayAgentTag *a = t->agents; a; a = a->next)
         t->pass->act(a, thread_context->context);
 }
 
-static void _dummy_act_func(TreeActTask *t, TayThreadContext *thread_context) {}
+static void _dummy_act_func(ActTask *t, TayThreadContext *thread_context) {}
 
-static void _traverse_actors(Node *node, TayPass *pass) {
-    TreeActTask task;
+static void _traverse_actors(Cell *node, TayPass *pass) {
+    ActTask task;
     _init_tree_act_task(&task, pass, node->first[pass->act_group]);
     tay_thread_set_task(0, _act_func, &task, pass->context);
     for (int i = 1; i < runner.count; ++i)
-        tay_thread_set_task(i, _dummy_act_func, 0, 0); // TODO: this is horrible
+        tay_thread_set_task(i, _dummy_act_func, 0, 0); // TODO: this is horrible, act here is still not parallelized!!!
     tay_runner_run_no_threads();
     if (node->lo)
         _traverse_actors(node->lo, pass);
@@ -295,10 +296,10 @@ static void _traverse_actors(Node *node, TayPass *pass) {
         _traverse_actors(node->hi, pass);
 }
 
-static void _act(TayState *state, int pass_index) { // TaySpaceContainer *container, TayPass *pass) {
-    Tree *t = state->space.storage; // container->storage;
+static void _act(TayState *state, int pass_index) {
+    Space *s = state->space.storage;
     TayPass *p = state->passes + pass_index;
-    _traverse_actors(t->nodes, p);
+    _traverse_actors(s->cells, p);
 }
 
 void space_tree_init(TaySpaceContainer *container, int dims, float *radii, int max_depth_correction) {
