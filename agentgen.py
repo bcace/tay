67be96c6cf4b789b3488e1cc00fa13c0dc5eed69
bcace <branimir.cace@gmail.com>
2020-12-31 16:06:48 +0100

    tay: preproc module to agentgen module

---
+++ agentgen.py
@@ -0,0 +1,123 @@
+import sys
+
+
+builtins_h = """float4 float4_null();
+float4 float4_make(float x, float y, float z);
+float4 float4_add(float4 a, float4 b);
+float4 float4_sub(float4 a, float4 b);
+float4 float4_div_scalar(float4 a, float s);
+"""
+
+builtins_c = """float4 float4_null() {
+    float4 r;
+    r.x = 0.0f;
+    r.y = 0.0f;
+    r.z = 0.0f;
+    return r;
+}
+
+float4 float4_make(float x, float y, float z) {
+    float4 r;
+    r.x = x;
+    r.y = y;
+    r.z = z;
+    return r;
+}
+
+float4 float4_add(float4 a, float4 b) {
+    float4 r;
+    r.x = a.x + b.x;
+    r.y = a.y + b.y;
+    r.z = a.z + b.z;
+    return r;
+}
+
+float4 float4_sub(float4 a, float4 b) {
+    float4 r;
+    r.x = a.x - b.x;
+    r.y = a.y - b.y;
+    r.z = a.z - b.z;
+    return r;
+}
+
+float4 float4_div_scalar(float4 a, float s) {
+    float4 r;
+    r.x = a.x / s;
+    r.y = a.y / s;
+    r.z = a.z / s;
+    return r;
+}
+"""
+
+def _read(path):
+    with open(path, "r") as f:
+        return f.read()
+
+def main(proj_dir):
+    agent_h = _read('%s/preproc/agent.h' % proj_dir)
+    agent_c = _read('%s/preproc/agent.c' % proj_dir)
+
+    #
+    # OpenCL source string
+    #
+
+    opencl_string = """
+{0}
+{1}
+{2}
+""".format(
+    agent_h,
+    builtins_c,
+    agent_c
+).replace('__PACK__', '__attribute__((packed))').replace('__GLOBAL__', 'global').replace('\n', '\\n\\\n')
+
+    #
+    # Write header file
+    #
+
+    with open('%s/agent.h' % proj_dir, 'w+') as f:
+        f.write("""#ifndef tay_agent_h
+#define tay_agent_h
+
+#include "tay.h"
+
+
+#pragma pack(push, 1)
+
+{0}
+void agent_see(Agent *a, Agent *b, SeeContext *context);
+void agent_act(Agent *agent, ActContext *context);
+
+{1}
+extern const char *agent_kernels_source;
+
+#pragma pack(pop)
+
+#endif
+""".format(
+    agent_h,
+    builtins_h
+    ).replace('__PACK__ ', '')
+)
+
+    #
+    # Write source file
+    #
+
+    with open('%s/agent.c' % proj_dir, 'w+') as f:
+        f.write("""#include "agent.h"
+
+
+{0}
+{1}
+const char *agent_kernels_source = "{2}";
+""".format(
+    agent_c.replace('__GLOBAL__ ', ''),
+    builtins_c,
+    opencl_string
+    )
+)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1])
