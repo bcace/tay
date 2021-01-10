5ca86e7e31e5b17c147d0ced16397ec796217704
bcace <branimir.cace@gmail.com>
2021-01-10 22:08:12 +0100

    benchmark: cleanup

--- benchmark/main.c
+++ benchmark/main.c
@@ -1,11 +1,277 @@
-#include "test.h"
+#include "tay.h"
+#include "agent.h"
 #include "thread.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
 
+
+static void _make_randomized_direction_cluster(TayState *state, int group, int count, float3 min, float3 max, float velocity) {
+    for (int i = 0; i < count; ++i) {
+        Agent *a = tay_get_available_agent(state, group);
+        a->p.x = min.x + rand() * (max.x - min.x) / (float)RAND_MAX;
+        a->p.y = min.y + rand() * (max.y - min.y) / (float)RAND_MAX;
+        a->p.z = min.z + rand() * (max.z - min.z) / (float)RAND_MAX;
+        a->v.x = -0.5f + rand() / (float)RAND_MAX;
+        a->v.y = -0.5f + rand() / (float)RAND_MAX;
+        a->v.z = -0.5f + rand() / (float)RAND_MAX;
+        float l = velocity / sqrtf(a->v.x * a->v.x + a->v.y * a->v.y + a->v.z * a->v.z);
+        a->v.x *= l;
+        a->v.y *= l;
+        a->v.z *= l;
+        a->b_buffer.x = 0.0f;
+        a->b_buffer.y = 0.0f;
+        a->b_buffer.z = 0.0f;
+        a->b_buffer_count = 0;
+        a->f_buffer.x = 0.0f;
+        a->f_buffer.y = 0.0f;
+        a->f_buffer.z = 0.0f;
+        tay_commit_available_agent(state, group);
+    }
+}
+
+static void _make_uniform_direction_cluster(TayState *state, int group, int count, float3 min, float3 max, float velocity) {
+    float3 v;
+    v.x = -0.5f + rand() / (float)RAND_MAX;
+    v.y = -0.5f + rand() / (float)RAND_MAX;
+    v.z = -0.5f + rand() / (float)RAND_MAX;
+    float l = velocity / sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+    v.x *= l;
+    v.y *= l;
+    v.z *= l;
+    for (int i = 0; i < count; ++i) {
+        Agent *a = tay_get_available_agent(state, group);
+        a->p.x = min.x + rand() * (max.x - min.x) / (float)RAND_MAX;
+        a->p.y = min.y + rand() * (max.y - min.y) / (float)RAND_MAX;
+        a->p.z = min.z + rand() * (max.z - min.z) / (float)RAND_MAX;
+        a->v.x = v.x;
+        a->v.y = v.y;
+        a->v.z = v.z;
+        a->b_buffer.x = 0.0f;
+        a->b_buffer.y = 0.0f;
+        a->b_buffer.z = 0.0f;
+        a->b_buffer_count = 0;
+        a->f_buffer.x = 0.0f;
+        a->f_buffer.y = 0.0f;
+        a->f_buffer.z = 0.0f;
+        tay_commit_available_agent(state, group);
+    }
+}
+
+static inline void _check_error(float a, float b, float *max_error) {
+    float relative_error = (a - b) / a;
+    if (relative_error > *max_error)
+        *max_error = relative_error;
+    else if (-relative_error > *max_error)
+        *max_error = -relative_error;
+}
+
+typedef struct {
+    float4 data[10000000];
+    int first_time;
+} Results;
+
+static Results *_create_results() {
+    Results *r = malloc(sizeof(Results));
+    r->first_time = 1;
+    return r;
+}
+
+static void _reset_results(Results *r) {
+    if (r)
+        r->first_time = 1;
+}
+
+static void _destroy_results(Results *r) {
+    if (r)
+        free(r);
+}
+
+static void _write_or_compare_results(Results *results, TayState *tay, int group, int agents_count) {
+    if (results) {
+        if (results->first_time) {
+            for (int i = 0; i < agents_count; ++i) {
+                Agent *agent = tay_get_agent(tay, group, i);
+                results->data[i] = agent->f_buffer;
+            }
+            results->first_time = 0;
+        }
+        else {
+            float max_error = 0.0f;
+
+            for (int i = 0; i < agents_count; ++i) {
+                Agent *agent = tay_get_agent(tay, group, i);
+                float4 a = results->data[i];
+                float4 b = agent->f_buffer;
+                _check_error(a.x, b.x, &max_error);
+                _check_error(a.y, b.y, &max_error);
+                _check_error(a.z, b.z, &max_error);
+            }
+
+            if (max_error > 0.0f); {
+                printf("    max error: %g\n", max_error);
+                if (max_error > 0.01f)
+                    assert(0);
+            }
+        }
+    }
+}
+
+typedef enum {
+    MC_UNIFORM,
+    MC_UNIFORM_WITH_ONE_CLUMP,
+} ModelCase;
+
+static void _test(ModelCase model_case, TaySpaceType space_type, float see_radius, int depth_correction, Results *results, int steps) {
+    srand(1);
+
+    int dims = 3;
+    int agents_count = 10000;
+    float space_size = 1000.0f;
+    float velocity = 1.0f;
+
+    float4 see_radii = { see_radius, see_radius, see_radius, 0.0f };
+
+    TayState *tay = tay_create_state(dims, see_radii);
+    tay_set_source(tay, agent_kernels_source);
+
+    ActContext act_context;
+    act_context.min.x = 0.0f;
+    act_context.min.y = 0.0f;
+    act_context.min.z = 0.0f;
+    act_context.max.x = space_size;
+    act_context.max.y = space_size;
+    act_context.max.z = space_size;
+
+    SeeContext see_context;
+    see_context.radii.x = see_radius;
+    see_context.radii.y = see_radius;
+    see_context.radii.z = see_radius;
+
+    int group = tay_add_group(tay, sizeof(Agent), agents_count);
+    tay_add_see(tay, group, group, agent_see, "agent_see", see_radii, &see_context, sizeof(see_context));
+    tay_add_act(tay, group, agent_act, "agent_act", &act_context, sizeof(act_context));
+
+    switch (model_case) {
+        case MC_UNIFORM: {
+            _make_randomized_direction_cluster(tay,
+                                               group,
+                                               agents_count,
+                                               float3_make(0.0f, 0.0f, 0.0f),
+                                               float3_make(space_size, space_size, space_size),
+                                               velocity);
+        } break;
+        case MC_UNIFORM_WITH_ONE_CLUMP: {
+            int clump_count = (int)floor(agents_count * 0.2);
+            _make_randomized_direction_cluster(tay,
+                                               group,
+                                               agents_count - clump_count,
+                                               float3_make(0.0f, 0.0f, 0.0f),
+                                               float3_make(space_size, space_size, space_size),
+                                               velocity);
+            _make_uniform_direction_cluster(tay,
+                                            group,
+                                            clump_count,
+                                            float3_make(0.0f, 0.0f, 0.0f),
+                                            float3_make(space_size * 0.1f, space_size * 0.1f, space_size * 0.1f),
+                                            velocity);
+        } break;
+        default:
+            assert(false); /* not implemented */
+    }
+
+    printf("    depth_correction: %d\n", depth_correction);
+
+    tay_simulation_start(tay);
+    double ms = tay_run(tay, steps, space_type, depth_correction);
+    printf("    milliseconds per frame: %g\n", ms);
+#if TAY_TELEMETRY
+    tay_threads_report_telemetry(0);
+#endif
+    tay_simulation_end(tay);
+
+    _write_or_compare_results(results, tay, group, agents_count);
+
+    tay_destroy_state(tay);
+}
 
 int main() {
     tay_runner_init();
     tay_runner_start_threads(8);
-    test();
+
+#if 1
+    Results *results = _create_results();
+#else
+    Results *results = 0;
+#endif
+
+    int steps = 100;
+    int model_case = MC_UNIFORM;
+
+    int beg_see_radius = 0;
+    int end_see_radius = 2;
+
+    int beg_depth_correction = 0;
+    int end_depth_correction = 2;
+
+    bool run_cpu_simple = false;
+    bool run_cpu_tree = true;
+    bool run_cpu_gird = true;
+    bool run_gpu_simple_direct = false;
+    bool run_gpu_simple_indirect = false;
+    bool run_gpu_tree = false;
+    bool run_cycling = false;
+
+    for (int i = beg_see_radius; i < end_see_radius; ++i) {
+        float see_radius = 50.0f * (1 << i);
+
+        printf("see radius: %.2f\n", see_radius);
+
+        if (run_cpu_simple) {
+            printf("  cpu simple:\n");
+            _test(model_case, TAY_SPACE_CPU_SIMPLE, see_radius, 0, results, steps);
+        }
+
+        if (run_cpu_tree) {
+            printf("  cpu tree:\n");
+            for (int j = beg_depth_correction; j < end_depth_correction; ++j)
+                _test(model_case, TAY_SPACE_CPU_TREE, see_radius, j, results, steps);
+        }
+
+        if (run_cpu_gird) {
+            printf("  cpu grid:\n");
+            for (int j = beg_depth_correction; j < end_depth_correction; ++j)
+                _test(model_case, TAY_SPACE_CPU_GRID, see_radius, j, results, steps);
+        }
+
+        if (run_gpu_simple_direct) {
+            printf("  gpu simple direct:\n");
+            _test(model_case, TAY_SPACE_GPU_SIMPLE_DIRECT, see_radius, 0, results, steps);
+        }
+
+        if (run_gpu_simple_indirect) {
+            printf("  gpu simple indirect:\n");
+            _test(model_case, TAY_SPACE_GPU_SIMPLE_INDIRECT, see_radius, 0, results, steps);
+        }
+
+        if (run_gpu_tree) {
+            printf("  gpu tree:\n");
+            for (int j = beg_depth_correction; j < end_depth_correction; ++j)
+                _test(model_case, TAY_SPACE_GPU_TREE, see_radius, j, results, steps);
+        }
+
+        if (run_cycling) {
+            printf("  cycling:\n");
+            for (int j = beg_depth_correction; j < end_depth_correction; ++j)
+                _test(model_case, TAY_SPACE_CYCLE_ALL, see_radius, j, results, steps);
+        }
+
+        _reset_results(results);
+    }
+
+    _destroy_results(results);
+
     tay_runner_stop_threads();
     return 0;
 }
--- benchmark/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef tay_test_h
-#define tay_test_h
-
-
-void test();
-
-#endif
