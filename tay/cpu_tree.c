ad4da3dd945af52b69e1bd8bd3a4112ea3a3a9a2
bcace <branimir.cace@gmail.com>
2021-04-04 10:42:11 +0200

    tay: cleanup

--- tay/cpu_tree.c
+++ tay/cpu_tree.c
@@ -6,11 +6,11 @@
 
 
 typedef struct TreeCell {
-    struct TreeCell *lo, *hi;       /* child partitions */
+    struct TreeCell *lo, *hi;       // child partitions
     struct TreeCell *thread_next;
-    TayAgentTag *first;             /* agents contained in this cell */
-    unsigned count;                 /* agent counts for this cell */
-    int dim;                        /* dimension along which the cell's partition is divided into child partitions */
+    TayAgentTag *first;             // agents contained in this cell
+    unsigned count;                 // agent counts for this cell
+    int dim;                        // dimension along which the cell's partition is divided into child partitions
     Box box;
     float mid;
 } TreeCell;
@@ -31,7 +31,7 @@ static void _thread_traverse_seen(CpuTree *tree, TreeCell *seer_cell, TreeCell *
     for (int i = 0; i < tree->dims; ++i)
         if (seer_box.min.arr[i] > seen_cell->box.max.arr[i] || seer_box.max.arr[i] < seen_cell->box.min.arr[i])
             return;
-    if (seen_cell->first) /* if there are any "seen" agents */
+    if (seen_cell->first)
         pass->pairing_func(seer_cell->first, seen_cell->first, pass->see, pass->radii, tree->dims, thread_context);
     if (seen_cell->lo)
         _thread_traverse_seen(tree, seer_cell, seen_cell->lo, pass, seer_box, thread_context);
@@ -40,14 +40,14 @@ static void _thread_traverse_seen(CpuTree *tree, TreeCell *seer_cell, TreeCell *
 }
 
 static void _see_func(SeeTask *task, TayThreadContext *thread_context) {
-    for (TreeCell *cell = task->first_cell; cell; cell = cell->thread_next) { /* seer cells */
-        if (cell->first) {
-            Box seer_box = cell->box;
+    for (TreeCell *seer_cell = task->first_cell; seer_cell; seer_cell = seer_cell->thread_next) {
+        if (seer_cell->first) {
+            Box seer_box = seer_cell->box;
             for (int i = 0; i < task->tree->dims; ++i) {
                 seer_box.min.arr[i] -= task->pass->radii.arr[i];
                 seer_box.max.arr[i] += task->pass->radii.arr[i];
             }
-            _thread_traverse_seen(task->tree, cell, task->tree->cells, task->pass, seer_box, thread_context);
+            _thread_traverse_seen(task->tree, seer_cell, task->tree->cells, task->pass, seer_box, thread_context);
         }
     }
 }
@@ -60,7 +60,7 @@ void cpu_tree_see(TayPass *pass) {
         Space *space = pass->seer_space;
         CpuTree *tree = &space->cpu_tree;
 
-        /* reset tasks */
+        // reset tasks
         for (int i = 0; i < runner.count; ++i) {
             tasks[i].first_cell = 0;
             tasks[i].agents_count = 0;
@@ -69,7 +69,7 @@ void cpu_tree_see(TayPass *pass) {
 
         TreeCell *buckets[TAY_MAX_BUCKETS] = { 0 };
 
-        /* sort cells into buckets wrt number of contained agents */
+        // sort cells into buckets wrt number of contained agents
         for (int i = 0; i < tree->cells_count; ++i) {
             TreeCell *cell = tree->cells + i;
             unsigned count = cell->count;
@@ -80,19 +80,19 @@ void cpu_tree_see(TayPass *pass) {
             }
         }
 
-        /* distribute cells among threads */
+        // distribute cells among threads
         for (int bucket_i = 0; bucket_i < TAY_MAX_BUCKETS; ++bucket_i) {
             TreeCell *cell = buckets[bucket_i];
 
             while (cell) {
                 TreeCell *next_cell = cell->thread_next;
 
-                SeeTask *task = sorted_tasks[0]; /* always take the task with fewest agents */
+                SeeTask *task = sorted_tasks[0]; // always take the task with fewest agents
                 cell->thread_next = task->first_cell;
                 task->first_cell = cell;
                 task->agents_count += cell->count;
 
-                /* sort the task wrt its number of agents */
+                // sort the task wrt its number of agents
                 {
                     int index = 1;
                     for (; index < runner.count && task->agents_count > sorted_tasks[index]->agents_count; ++index);
@@ -105,7 +105,7 @@ void cpu_tree_see(TayPass *pass) {
             }
         }
 
-        /* set tasks */
+        // set tasks
         for (int i = 0; i < runner.count; ++i) {
             SeeTask *task = tasks + i;
             _init_tree_see_task(task, tree, pass, i);
@@ -119,7 +119,7 @@ void cpu_tree_see(TayPass *pass) {
 typedef struct {
     CpuTree *tree;
     TayPass *pass;
-    int counter; /* tree cell skipping counter so each thread processes different seer nodes */
+    int counter; // tree cell skipping counter so each thread processes different seer nodes
 } ActTask;
 
 static void _init_tree_act_task(ActTask *task, CpuTree *tree, TayPass *pass, int thread_index) {
@@ -130,7 +130,7 @@ static void _init_tree_act_task(ActTask *task, CpuTree *tree, TayPass *pass, int
 
 static void _thread_traverse_actors(ActTask *task, TreeCell *cell, TayThreadContext *thread_context) {
     if (task->counter % runner.count == 0) {
-        if (cell->first) { /* if there are any "seeing" agents */
+        if (cell->first) { // if there are any "seeing" agents
             for (TayAgentTag *tag = cell->first; tag; tag = tag->next)
                 task->pass->act(tag, thread_context->context);
         }
@@ -189,7 +189,7 @@ static inline void _decide_cell_split(TreeCell *cell, int dims, int *max_depths,
 
 static void _sort_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *agent, Depths cell_depths, float *radii) {
 
-    if (cell->dim == TREE_CELL_LEAF_DIM) { /* leaf cell, put the agent here */
+    if (cell->dim == TREE_CELL_LEAF_DIM) { // leaf cell, put the agent here
         agent->next = cell->first;
         cell->first = agent;
         ++cell->count;
@@ -201,9 +201,9 @@ static void _sort_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *agent,
 
     float pos = float4_agent_position(agent).arr[cell->dim];
     if (pos < cell->mid) {
-        if (cell->lo == 0) { /* lo cell needed but doesn't exist yet */
+        if (cell->lo == 0) { // lo cell needed but doesn't exist yet
 
-            if (tree->cells_count == tree->max_cells) { /* no more space for new cells, leave the agent in current cell */
+            if (tree->cells_count == tree->max_cells) { // no more space for new cells, leave the agent in current cell
                 agent->next = cell->first;
                 cell->first = agent;
                 ++cell->count;
@@ -219,9 +219,9 @@ static void _sort_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *agent,
         _sort_point_agent(tree, cell->lo, agent, sub_node_depths, radii);
     }
     else {
-        if (cell->hi == 0) { /* hi cell needed but doesn't exist yet */
+        if (cell->hi == 0) { // hi cell needed but doesn't exist yet
 
-            if (tree->cells_count == tree->max_cells) { /* no more space for new cells, leave the agent in current cell */
+            if (tree->cells_count == tree->max_cells) { // no more space for new cells, leave the agent in current cell
                 agent->next = cell->first;
                 cell->first = agent;
                 ++cell->count;
@@ -240,7 +240,7 @@ static void _sort_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *agent,
 
 static void _sort_non_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *agent, Depths cell_depths, float *radii) {
 
-    if (cell->dim == TREE_CELL_LEAF_DIM) { /* leaf cell, put the agent here */
+    if (cell->dim == TREE_CELL_LEAF_DIM) { // leaf cell, put the agent here
         agent->next = cell->first;
         cell->first = agent;
         ++cell->count;
@@ -253,9 +253,9 @@ static void _sort_non_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *ag
     float min = float4_agent_min(agent).arr[cell->dim];
     float max = float4_agent_max(agent).arr[cell->dim];
     if (max <= cell->mid) {
-        if (cell->lo == 0) { /* lo cell needed but doesn't exist yet */
+        if (cell->lo == 0) { // lo cell needed but doesn't exist yet
 
-            if (tree->cells_count == tree->max_cells) { /* no more space for new cells, leave the agent in current cell */
+            if (tree->cells_count == tree->max_cells) { // no more space for new cells, leave the agent in current cell
                 agent->next = cell->first;
                 cell->first = agent;
                 ++cell->count;
@@ -271,9 +271,9 @@ static void _sort_non_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *ag
         _sort_non_point_agent(tree, cell->lo, agent, sub_node_depths, radii);
     }
     else if (min >= cell->mid) {
-        if (cell->hi == 0) { /* hi cell needed but doesn't exist yet */
+        if (cell->hi == 0) { // hi cell needed but doesn't exist yet
 
-            if (tree->cells_count == tree->max_cells) { /* no more space for new cells, leave the agent in current cell */
+            if (tree->cells_count == tree->max_cells) { // no more space for new cells, leave the agent in current cell
                 agent->next = cell->first;
                 cell->first = agent;
                 ++cell->count;
@@ -288,7 +288,7 @@ static void _sort_non_point_agent(CpuTree *tree, TreeCell *cell, TayAgentTag *ag
         }
         _sort_non_point_agent(tree, cell->hi, agent, sub_node_depths, radii);
     }
-    else { /* agent extends to both sides of the mid plane, leave it in the current cell */
+    else { // agent extends to both sides of the mid plane, leave it in the current cell
         agent->next = cell->first;
         cell->first = agent;
         ++cell->count;
@@ -318,18 +318,18 @@ void cpu_tree_sort(TayGroup *group) {
     Space *space = &group->space;
     CpuTree *tree = &space->cpu_tree;
 
-    /* calculate max partition depths for each dimension */
+    // calculate max partition depths for each dimension
     Depths root_cell_depths;
     for (int i = 0; i < tree->dims; ++i) {
         tree->max_depths.arr[i] = _max_depth(space->box.max.arr[i] - space->box.min.arr[i], space->radii.arr[i] * 2.0f);
         root_cell_depths.arr[i] = 0;
     }
 
-    /* set up root cell */
+    // set up root cell
     {
         tree->cells_count = 1;
         _tree_clear_cell(tree->cells);
-        tree->cells->box = space->box; /* root cell inherits tree's box */
+        tree->cells->box = space->box; // root cell inherits tree's box
         _decide_cell_split(tree->cells, tree->dims, tree->max_depths.arr, space->radii.arr, root_cell_depths);
     }
 
