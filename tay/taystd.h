f2c988065b3df08525891104890f9bdcb35a4e1c
bcace <branimir.cace@gmail.com>
2021-04-20 21:28:56 +0200

    tay: added the taystd module

---
+++ tay/taystd.h
@@ -0,0 +1,22 @@
+#ifndef taystd_h
+#define taystd_h
+
+
+float3 float3_null();
+float3 float3_make(float x, float y, float z);
+float3 float3_add(float3 a, float3 b);
+float3 float3_sub(float3 a, float3 b);
+float3 float3_div_scalar(float3 a, float s);
+float3 float3_mul_scalar(float3 a, float s);
+float3 float3_normalize(float3 a);
+float3 float3_normalize_to(float3 a, float b);
+float float3_length(float3 a);
+float float3_dot(float3 a, float3 b);
+
+float4 float4_null();
+float4 float4_make(float x, float y, float z, float w);
+float4 float4_add(float4 a, float4 b);
+float4 float4_sub(float4 a, float4 b);
+float4 float4_div_scalar(float4 a, float s);
+
+#endif
