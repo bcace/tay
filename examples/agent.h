4fc90c7a90a77d5f06fd1ff10520c68171978bdc
bcace <branimir.cace@gmail.com>
2021-04-20 21:29:28 +0200

    examples: removed preproc and using taystd

--- examples/agent.h
+++ examples/agent.h
@@ -70,33 +70,12 @@ typedef struct SphContext {
     float viscosity;
 } SphContext;
 
+#pragma pack(pop)
+
 void sph_particle_density(SphParticle *a, SphParticle *b, SphContext *c);
 void sph_particle_pressure(SphParticle *a, SphContext *c);
 void sph_force_terms(SphParticle *a, SphParticle *b, SphContext *c);
 void sph_particle_leapfrog(SphParticle *a, SphContext *c);
 void sph_particle_reset(SphParticle *a);
-
-
-float3 float3_null();
-float3 float3_make(float x, float y, float z);
-float3 float3_add(float3 a, float3 b);
-float3 float3_sub(float3 a, float3 b);
-float3 float3_mul(float3 a, float3 b);
-float3 float3_div_scalar(float3 a, float s);
-float3 float3_mul_scalar(float3 a, float s);
-float3 float3_normalize(float3 a);
-float3 float3_normalize_to(float3 a, float b);
-float float3_length(float3 a);
-float float3_dot(float3 a, float3 b);
-
-float4 float4_null();
-float4 float4_make(float x, float y, float z, float w);
-float4 float4_add(float4 a, float4 b);
-float4 float4_sub(float4 a, float4 b);
-float4 float4_div_scalar(float4 a, float s);
-
-extern const char *agent_kernels_source;
-
-#pragma pack(pop)
 
 #endif
