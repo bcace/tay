ca5cbe81c099ba0c33271fd8a36cc78f398ef685
bcace <branimir.cace@gmail.com>
2021-04-26 18:48:33 +0200

    benchmark: preparing for cached aabb tree

--- benchmark/main.c
+++ benchmark/main.c
@@ -13,23 +13,23 @@ int main() {
 
     Results *results = results_create();
 
-    test_basic(results, MC_UNIFORM, 1000,
-               0, 1, // see radius
-               0, 10, // depth correction
-               // TAY_CPU_SIMPLE
-               // |
-               TAY_CPU_GRID
-               // |
-               // TAY_CPU_KD_TREE
-    );
-
-    // test_nonpoint(results, 100,
-    //               0, 1, // see radius
-    //               0, 1, // depth correction
-    //               // TAY_CPU_SIMPLE |
-    //               TAY_CPU_KD_TREE |
-    //               TAY_CPU_AABB_TREE
+    // test_basic(results, MC_UNIFORM, 1000,
+    //            0, 1, // see radius
+    //            0, 10, // depth correction
+    //            // TAY_CPU_SIMPLE
+    //            // |
+    //            TAY_CPU_GRID
+    //            // |
+    //            // TAY_CPU_KD_TREE
     // );
+
+    test_nonpoint(results, 1000,
+                  0, 1, // see radius
+                  0, 1, // depth correction
+                  // TAY_CPU_SIMPLE |
+                  TAY_CPU_KD_TREE |
+                  TAY_CPU_AABB_TREE
+    );
 
     // TaySpaceType spec_pairs[] = {
     //     // TAY_CPU_SIMPLE, TAY_CPU_SIMPLE,
